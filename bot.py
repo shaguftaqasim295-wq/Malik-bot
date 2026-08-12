51ddfe71f46ff3328385aeff903fee5d8662b4a3
shaguftaqasim295-wq <shaguftaqasim295@gmail.com>
2026-08-12 21:49:23 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,20 +1,14 @@
 import requests
 
-TELEGRAM_BOT_TOKEN = "8689746853:AAHgj8KPZ6jUcejQ7vKmv_jcAjhwUMAz-3Q"
+TELEGRAM_BOT_TOKEN = "8689746853:AAHgj8KPZ6jUcejQ7vKmv_jcAjhwUMAZ-3Q"
 CHANNEL_CHAT_ID = "-1006927353586"
 
-def send_test_message():
-    url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
-    payload = {
-        'chat_id': CHANNEL_CHAT_ID,
-        'text': "🟢 *System is running...*\n\nBot successfully connect ho gaya hai!",
-        'parse_mode': 'Markdown'
-    }
-    try:
-        response = requests.post(url, json=payload, timeout=10)
-        print("Response:", response.text)
-    except Exception as e:
-        print("Error:", e)
+url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
+payload = {
+    'chat_id': CHANNEL_CHAT_ID,
+    'text': "Test message from script"
+}
 
-if __name__ == "__main__":
-    send_test_message()
+response = requests.post(url, json=payload)
+print("TELEGRAM STATUS CODE:", response.status_code)
+print("TELEGRAM RESPONSE:", response.text)
